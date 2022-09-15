bbe91e8b55a2773ec404b689a0d9424fad602f2f
Felipe de Jesus Lopez <isc19350301@gmail.com>
2022-09-15 10:46:26 -0500

    HolaMundoEjem

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = requiere ('express')
+
+class Server{
+constructor(){
+   this.app = express()
+}
+routes(){
+
+this.app.get{'/', (req,res) =>{
+ res.send('Hello WOrld')
+}
+}
+listen(){
+this.app.listen(process.env.PORT, ()=>)
+console.log{"Backend en ejecucion eb ek puerto", process.env.PORT 
+
+}
+
+}
+
+}
+module.exports=Server
